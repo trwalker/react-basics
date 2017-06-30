208af8f3aa1a4929d654f11daa262aa03b977cef
trwalker <tim.r.walker@gmail.com>
2017-06-30 06:23:50 -0700

    adding count func

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,11 +2,24 @@ import React from 'react';
 import Button from './forms/Button.jsx';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { count: 0 };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState({
+           count: this.state.count + 1
+        });
+    };
+
     render() {
         return (
             <div>
                 <h2>Hello World</h2>
-                { Button({ title: "Go Now, Now Go"}) }
+                { Button({ count: this.state.count, handleClick: this.handleClick }) }
             </div>
         );
     }
--- client/components/forms/Button.jsx
+++ client/components/forms/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 
-const Button = ({title}) => {
+const Button = ({count, handleClick}) => {
     return (
-        <button>{title}</button>
+        <button onClick={handleClick}>{count}</button>
     );
 };
 
