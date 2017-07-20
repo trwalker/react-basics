42f86cbbcd73703a863d02e86c5c571f1e239be8
trwalker <tim.r.walker@gmail.com>
2017-07-20 07:12:27 -0700

    Fixing card col by breakpoint

--- client/components/cards/Card.jsx
+++ client/components/cards/Card.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Card = (props) => {
     return (
-        <div className="col-4">
-            <div className="card" style={{width: '20rem'}}>
-                <img className="card-img-top" src={props.iconUrl} width="318" height="318" alt="Card image cap" />
+        <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+            <div className="card" style={{width: '100%'}}>
+                <img className="card-img-top" src={props.iconUrl} alt="Card image cap" />
                 <div className="card-block">
                     <h4 className="card-title">{props.name}</h4>
                     <p className="card-text">{props.company}</p>
